fe892fe60993caa3aa3b42b3b119f557f2eb579b
jvras58 <jvras@cin.ufpe.br>
2025-01-16 18:14:36 -0300

    Atualiza variáveis de ambiente no .env-sample, ajusta configuração do docker-compose.yml para incluir verificação de saúde do serviço PostgreSQL e modifica o script de seed para adicionar novos usuários e posts mockados.

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,7 +1,6 @@
 const { PrismaClient } = require('@prisma/client');
 const bcrypt = require('bcryptjs');
 require('dotenv/config');
-
 // TODO: PARA ME EXECUTAR: 
 // npm run seed
 // ERRO Cannot find module: dotenv/bcryptjs:
@@ -13,10 +12,18 @@ const prisma = new PrismaClient();
 async function main() {
   const users = [
     { 
+      id: "author1",
       name: process.env.NAME_STRING, 
       email: process.env.EMAIL_STRING, 
       password: process.env.ADMIN_PASSWORD,
       role: "ADMIN"
+    },
+    {
+      id: "author2",
+      name: "Jane Smith",
+      email: "jane.smith@example.com",
+      password: "password123",
+      role: "DEFAULT"
     }
   ];
 
@@ -24,6 +31,7 @@ async function main() {
     const hashedPassword = await bcrypt.hash(user.password, 10);
 
     const userData = {
+      id: user.id,
       name: user.name,
       password: hashedPassword,
       email: user.email,
@@ -35,34 +43,29 @@ async function main() {
     });
   }
 
-    const mockPosts = [
-        {
-        id: "1",
-        author: {
-            id: "author1",
-            name: "John Doe"
-        },
-        title: "Introdução ao React",
-        description: "Uma introdução ao React para iniciantes",
-        content: "React é uma biblioteca JavaScript popular para construção de interfaces de usuário. Ele permite que os desenvolvedores criem componentes de UI reutilizáveis ​​que podem ser compostos para construir aplicativos complexos. Neste post, abordaremos os fundamentos do React, incluindo componentes, props e estado.",
-        category: "Frontend",
-        tags: ["React", "JavaScript", "Web Development"],
-        createdAt: "2023-05-15"
-        },
-        {
-            id: "2",
-            author: {
-              id: "author2",
-              name: "Jane Smith"
-            },
-            title: "O poder do Tailwind CSS",
-            description: "Como Tailwind CSS pode acelerar seu processo de desenvolvimento",
-            content: "Tailwind CSS é uma estrutura CSS utilitária que fornece classes utilitárias de baixo nível para construir designs personalizados. Ele permite o desenvolvimento rápido da UI sem sair do HTML. Esta postagem explora como o Tailwind pode acelerar significativamente seu processo de desenvolvimento e tornar suas folhas de estilo mais fáceis de manter.",
-            category: "CSS",
-            tags: ["CSS", "Tailwind", "Web Design"],
-            createdAt: "2023-05-20"
-        },
-    ]
+  const mockPosts = [
+    {
+      id: "1",
+      authorId: "author1",
+      title: "Introdução ao React",
+      description: "Uma introdução ao React para iniciantes",
+      content: "React é uma biblioteca JavaScript popular para construção de interfaces de usuário. Ele permite que os desenvolvedores criem componentes de UI reutilizáveis ​​que podem ser compostos para construir aplicativos complexos. Neste post, abordaremos os fundamentos do React, incluindo componentes, props e estado.",
+      category: "Frontend",
+      tags: ["React", "JavaScript", "Web Development"],
+      createdAt: "2023-05-15"
+    },
+    {
+      id: "2",
+      authorId: "author2",
+      title: "O poder do Tailwind CSS",
+      description: "Como Tailwind CSS pode acelerar seu processo de desenvolvimento",
+      content: "Tailwind CSS é uma estrutura CSS utilitária que fornece classes utilitárias de baixo nível para construir designs personalizados. Ele permite o desenvolvimento rápido da UI sem sair do HTML. Esta postagem explora como o Tailwind pode acelerar significativamente seu processo de desenvolvimento e tornar suas folhas de estilo mais fáceis de manter.",
+      category: "CSS",
+      tags: ["CSS", "Tailwind", "Web Design"],
+      createdAt: "2023-05-20"
+    },
+  ];
+
   for (const post of mockPosts) {
     await prisma.post.create({
       data: {
@@ -74,7 +77,7 @@ async function main() {
         tags: post.tags,
         createdAt: new Date(post.createdAt),
         author: {
-          connect: { id: post.author?.id }
+          connect: { id: post.authorId }
         }
       }
     });
